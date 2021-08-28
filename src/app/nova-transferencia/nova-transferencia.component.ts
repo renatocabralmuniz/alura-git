70c517ac7053cbcf93faf1b9462761bf413a52d5
Renato Cabral <renatocabralmuniz@gmail.com>
2021-08-28 01:21:34 -0300

    Primeiro commit bytebank

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-tranferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  // tslint:disable-next-line: typedef
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+
+  }
+}
